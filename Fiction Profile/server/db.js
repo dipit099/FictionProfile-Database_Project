fa6538af623f5be5942bd72b715edfd58fa85ee0
Dipit Saha <sdipit099@gmail.com>
2023-12-26 00:19:50 +0600

    check

--- Fiction Profile/server/db.js
+++ Fiction Profile/server/db.js
@@ -1,10 +1,15 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
+    // user: "postgres",
+    // password: "alliswell",
+    // host: "localhost",
+    // port: 5432,
+    // database: "Fiction Profile"
     user: "postgres",
-    password: "alliswell",
-    host: "localhost",
+    password: "dx$Q6*vWTnu!45j",
+    host: "db.eocmqhvplptwmzerrcng.supabase.co",
     port: 5432,
-    database: "Fiction Profile"
+    database: "postgres"
 });
 
 module.exports = pool;
--- Fiction Profile/server/index.js
+++ Fiction Profile/server/index.js
@@ -3,65 +3,64 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-
 app.use(cors());
 app.use(express.json());
 
-//ROUTES//
+// ROUTES //
 
-//create a todo
+// Create a todo
 app.post("/todos", async (req, res) => {
     try {
-        //console.log(req.body);
         const { description } = req.body;
         const newTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1) RETURNING *",
-            [description]);
+            'INSERT INTO "Fiction Profile"."todo" (description) VALUES($1) RETURNING *',
+            [description]
+        );
         res.json(newTodo.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-//get all todos
+// Get all todos
 app.get("/todos", async (req, res) => {
     try {
-        const allTodos = await pool.query("SELECT * FROM todo");
+        const allTodos = await pool.query('SELECT * FROM "Fiction Profile"."todo"');
         res.json(allTodos.rows);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-//get a todo
+// Get a todo
 app.get("/todos/:id", async (req, res) => {
     try {
-        //console.log(req.params);
         const { id } = req.params;
         const todo = await pool.query(
-            "SELECT * FROM todo WHERE todo_id = $1", [id]);
+            'SELECT * FROM "Fiction Profile"."todo" WHERE todo_id = $1',
+            [id]
+        );
         res.json(todo.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-//update a todo
+// Update a todo
 app.put("/todos/:id", async (req, res) => {
     try {
-        //console.log(req.params);
-        //console.log(req.body);
         const { id } = req.params;
         const { description } = req.body;
         const updateTodo = await pool.query(
-            "UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+            'UPDATE "Fiction Profile"."todo" SET description = $1 WHERE todo_id = $2',
+            [description, id]
+        );
         res.json("Todo was updated!");
     } catch (err) {
         console.error(err.message);
     }
 });
 
-
 app.listen(5197, () => {
-    console.log("server started on port 5197");
+    console.log("Server started on port 5197");
 });
