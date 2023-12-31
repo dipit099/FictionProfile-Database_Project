90fd2b380da12c9c7a4062503efaa466d2581f70
SMAM Mahdi <mahdithe8@gmail.com>
2023-12-31 23:28:11 +0600

    added python codes for data generation from tmdb database

---
+++ stuff/Data Generation/tmdb_all_columns.py
@@ -0,0 +1,25 @@
+import requests
+import pandas as pd
+
+# TMDb API endpoint for top-rated movies
+api_key = '03571ead73aa850cefb8a796b9547e91'
+api_url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}&sort_by=vote_count.desc&vote_count.gte=1000&page='
+
+all_movies = []
+
+# Fetch top 5000 movies (100 movies per page, 50 pages)
+for page_num in range(1, 51):
+    response = requests.get(api_url + str(page_num))
+    if response.status_code == 200:
+        movies_data = response.json().get('results')
+        all_movies.extend(movies_data)
+    else:
+        print(f"Failed to fetch data from page {page_num}")
+
+# Create DataFrame with all available columns
+movies_df = pd.DataFrame(all_movies)
+
+# Write data to Excel file
+excel_file = 'top_5000_movies_all_columns.xlsx'
+movies_df.to_excel(excel_file, index=False)
+print(f'Data extracted and saved to {excel_file}')
