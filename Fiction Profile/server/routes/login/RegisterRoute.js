1e0aae2aa672fc25270810262a02581e32d3a7cd
SMAM Mahdi <mahdithe8@gmail.com>
2024-01-21 22:21:12 +0600

    updated the registration logic

--- Fiction Profile/server/routes/login/RegisterRoute.js
+++ Fiction Profile/server/routes/login/RegisterRoute.js
@@ -34,8 +34,8 @@ router.post('/', upload.single('profilePicture'), async (req, res) => {
 
     try {
         // Check if the username or email already exists
-        const checkUserQuery = 'SELECT * FROM "Fiction Profile"."PEOPLE" WHERE username = $1 OR email = $2';
-        const checkUserResult = await pool.query(checkUserQuery, [userName, email]);
+        const checkUserQuery = 'SELECT * FROM "Fiction Profile"."PEOPLE" WHERE (username = $1 OR email = $2) AND role = $3';
+        const checkUserResult = await pool.query(checkUserQuery, [userName, email, role]);
         if (checkUserResult.rows.length > 0) {
             // User with the same username or email already exists
             console.log('Username or email already exists');
@@ -48,31 +48,38 @@ router.post('/', upload.single('profilePicture'), async (req, res) => {
         const hashedPassword = await bcrypt.hash(pass, salt);
         //console.log(hashedPassword);
 
+
+        // Here are some modifications needed
+        // We need to first insert the people and then retrieve their id to upload the pic
+        // So it is best to just have the upload pic functionality for Account edit section
+
         //setting up profile picture path
         const currentDate = new Date().toISOString().split('T')[0];
-        const getLastInsertedRowQuery = 'SELECT people_id FROM "Fiction Profile"."PEOPLE" ORDER BY people_id DESC LIMIT 1';
-        const lastInsertedRowResult = await pool.query(getLastInsertedRowQuery);
-        let lastId = 0;
-        if (lastInsertedRowResult.rows.length > 0) {
-            lastId = parseInt(lastInsertedRowResult.rows[0].people_id.slice(2));
-        }
-        const ppid = `pp${(lastId + 1).toString().padStart(6, '0')}`;
-        let profilePicturePath = null;
-        if (req.file) {
-            const storageRef = ref(storage, `uploads/${ppid + "_" + currentDate}`);
-            const metadata = {
-                contentType: req.file.mimetype,
-            };
-            // Upload the file in the bucket storage
-            const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
-            //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
-            // Grab the public url
-            const downloadURL = await getDownloadURL(snapshot.ref);
+        // const getLastInsertedRowQuery = 'SELECT people_id FROM "Fiction Profile"."PEOPLE" ORDER BY people_id DESC LIMIT 1';
+        // const lastInsertedRowResult = await pool.query(getLastInsertedRowQuery);
+        // let lastId = 0;
+        // if (lastInsertedRowResult.rows.length > 0) {
+        //     lastId = parseInt(lastInsertedRowResult.rows[0].people_id.slice(2));
+        // }
+        // const ppid = `pp${(lastId + 1).toString().padStart(6, '0')}`;
+        // let profilePicturePath = null;
+        // if (req.file) {
+        //     const storageRef = ref(storage, `uploads/${ppid + "_" + currentDate}`);
+        //     const metadata = {
+        //         contentType: req.file.mimetype,
+        //     };
+        //     // Upload the file in the bucket storage
+        //     const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+        //     //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
+        //     // Grab the public url
+        //     const downloadURL = await getDownloadURL(snapshot.ref);
 
-            console.log('File successfully uploaded.');
-            console.log('File available at', downloadURL);
-            profilePicturePath = downloadURL;
-        }
+        //     console.log('File successfully uploaded.');
+        //     console.log('File available at', downloadURL);
+        //     profilePicturePath = downloadURL;
+        // }
+
+        let profilePicturePath="";
 
         // If the username and email are unique, insert the new user into the database
         // const insertUserQuery =
