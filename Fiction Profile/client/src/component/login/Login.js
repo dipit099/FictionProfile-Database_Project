0af051765171c08108ffa3b3c36b91701ebed39f
Dipit Saha <sdipit099@gmail.com>
2024-01-23 14:40:13 +0600

    fav and add button

--- Fiction Profile/client/src/component/login/Login.js
+++ Fiction Profile/client/src/component/login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import './Login-register.css';
+
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
