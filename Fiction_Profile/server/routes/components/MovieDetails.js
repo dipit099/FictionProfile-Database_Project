a3372096cc1620eaf2f89a7cc703e600bbed112b
SMAM Mahdi <mahdithe8@gmail.com>
2024-02-20 21:26:09 +0600

    rating update

--- Fiction_Profile/server/routes/components/MovieDetails.js
+++ Fiction_Profile/server/routes/components/MovieDetails.js
@@ -7,7 +7,7 @@ router.get('/:id', async (req, res) => {
         console.log('Fetching movie details');
         const { id } = req.params;
         const result = await pool.query(
-            'SELECT id,title, poster_path,backdrop_path,release_date, vote_average, overview,original_language,genres FROM "Fiction Profile"."MOVIE" WHERE id = $1',
+            'SELECT id,title, poster_path,backdrop_path,release_date, (SELECT rating FROM "Fiction Profile"."MEDIA" where movie_id = $1) AS vote_count, overview,original_language,genres FROM "Fiction Profile"."MOVIE" WHERE id = $1',
             [id]
         );
 
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
             poster_path: `https://image.tmdb.org/t/p/original/${result.rows[0].poster_path}`,
             backdrop_path: `https://image.tmdb.org/t/p/original/${result.rows[0].backdrop_path}`,
             release_date: result.rows[0].release_date,
-            vote_average: result.rows[0].vote_average,
+            vote_average: result.rows[0].vote_count,
             overview: result.rows[0].overview,
             original_language: result.rows[0].original_language,
             genres: result.rows[0].genres
