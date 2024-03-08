b0203688047ab388b77547be1d797fdf880cef38
SMAM Mahdi <mahdithe8@gmail.com>
2024-03-08 15:04:24 +0600

    tt

--- Fiction_Profile/server/routes/moderator/ModeratorRoute.js
+++ Fiction_Profile/server/routes/moderator/ModeratorRoute.js
@@ -3,24 +3,21 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../../db");
 
+
 router.post("/add-media", async (req, res) => {
     try {
-        const { type, title, author } = req.body;
+        const { isbn, type, title, year, language, genre, runtime } = req.body;
 
-        // Add media to the database
-        await pool.query(
-            `INSERT INTO "Fiction Profile"."MEDIA" (type, title, author)
-            VALUES ($1, $2, $3)`,
-            [type, title, author]
-        );
+        console.log('So ws red');
 
         res.json({ message: "Media added successfully" });
     } catch (error) {
         console.error("Error adding media:", error);
         res.status(500).json({ error: "Internal server error" });
     }
-}
-);
+});
+
+module.exports = router;
 
 
 module.exports = router;
