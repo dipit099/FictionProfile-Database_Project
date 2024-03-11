ac8cd7dcc5aed3c2f761116beca645f95f768914
Dipit Saha <sdipit099@gmail.com>
2024-03-11 12:59:52 +0600

    final commit 01

--- Fiction_Profile/server/db.js
+++ Fiction_Profile/server/db.js
@@ -1,15 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    // user: "postgres",
-    // password: "alliswell",
-    // host: "localhost",
-    // port: 5432,
-    // database: "db"
-    user: "postgres.eocmqhvplptwmzerrcng",
-    password: "dx$Q6*vWTnu!45j",
-    host: "aws-0-ap-south-1.pooler.supabase.com",
-    port: 6543,
-    database: "postgres"
+    user: "postgres",
+    password: "alliswell",
+    host: "localhost",
+    port: 5432,
+    database: "db"   
 });
 
 module.exports = pool;
