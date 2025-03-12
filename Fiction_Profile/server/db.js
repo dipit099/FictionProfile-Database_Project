80d466860071f8755a67824b289634cc25d4de61
Dipit Saha <112118531+dipit099@users.noreply.github.com>
2025-03-12 19:46:27 +0600

    Update db.js

--- Fiction_Profile/server/db.js
+++ Fiction_Profile/server/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 const pool = new Pool({   
     host: "aws-0-ap-south-1.pooler.supabase.com",
-    port: 6543,
+    port: 5432,
     database: "postgres",
     user: process.env.DB_USER,   
     password: process.env.DB_PASSWORD,
