70ae1c2eac733f927eef2864f8b645cd3bbc3f09
Dipit Saha <112118531+dipit099@users.noreply.github.com>
2025-03-13 11:43:27 +0600

    Update HomeMediaRoute.js

--- Fiction_Profile/server/routes/home/HomeMediaRoute.js
+++ Fiction_Profile/server/routes/home/HomeMediaRoute.js
@@ -4,6 +4,7 @@ const pool = require("../../db");
 // Endpoint to get movie details by ID
 
 
+
 router.get('/movie', async (req, res) => {
     try {
         const people_id = req.query.people_id;
@@ -16,18 +17,23 @@ router.get('/movie', async (req, res) => {
                     media.poster_path,
                     media.rating,
                     media.vote_count,
-                    (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+                    COALESCE(fav.is_favorite, 0) AS is_favorite
                 FROM
                     "Fiction Profile"."MEDIA" media
+                LEFT JOIN (
+                    SELECT media_id, 1 AS is_favorite
+                    FROM "Fiction Profile"."FAVORITE"
+                    WHERE user_id = $1
+                ) fav ON fav.media_id = media.media_id
                 WHERE
                     media.movie_id IS NOT NULL
                     AND media.poster_path IS NOT NULL
                 ORDER BY
                     media.popularity DESC, media.rating DESC, media.vote_count DESC
                 LIMIT 8
-            `
-            ,[people_id]
-        ); 
+            `,
+            [people_id]
+        );
 
         const media = result.rows.map(mediaItem => ({
             id: mediaItem.movie_id,
@@ -37,7 +43,6 @@ router.get('/movie', async (req, res) => {
             vote_count: mediaItem.vote_count,
             is_favorite: mediaItem.is_favorite
         }));
-        
 
         res.json({ media });
     } catch (error) {
@@ -47,11 +52,8 @@ router.get('/movie', async (req, res) => {
 });
 
 
-
-
 router.get('/tv', async (req, res) => {
     try {
-        
         const people_id = req.query.people_id;
 
         const result = await pool.query(
@@ -62,16 +64,21 @@ router.get('/tv', async (req, res) => {
                     media.poster_path,
                     media.rating,
                     media.vote_count,
-                    (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+                    COALESCE(fav.is_favorite, 0) AS is_favorite
                 FROM
                     "Fiction Profile"."MEDIA" media
+                LEFT JOIN (
+                    SELECT media_id, 1 AS is_favorite
+                    FROM "Fiction Profile"."FAVORITE"
+                    WHERE user_id = $1
+                ) fav ON fav.media_id = media.media_id
                 WHERE
                     media.tv_id IS NOT NULL
                 ORDER BY
                     media.popularity DESC, media.rating DESC, media.vote_count DESC
                 LIMIT 8
-            `
-            ,[people_id]
+            `,
+            [people_id]
         );
 
         const media = result.rows.map(tvshow => ({
@@ -93,9 +100,8 @@ router.get('/tv', async (req, res) => {
 
 router.get('/book', async (req, res) => {
     try {
-
         const people_id = req.query.people_id;
-    
+
         const result = await pool.query(
             `   
                 SELECT
@@ -104,16 +110,21 @@ router.get('/book', async (req, res) => {
                     media.poster_path,
                     media.rating,
                     media.vote_count,
-                    (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+                    COALESCE(fav.is_favorite, 0) AS is_favorite
                 FROM
                     "Fiction Profile"."MEDIA" media
+                LEFT JOIN (
+                    SELECT media_id, 1 AS is_favorite
+                    FROM "Fiction Profile"."FAVORITE"
+                    WHERE user_id = $1
+                ) fav ON fav.media_id = media.media_id
                 WHERE
                     media.book_id IS NOT NULL
                 ORDER BY
                     media.popularity DESC, media.rating DESC, media.vote_count DESC
                 LIMIT 8
-            `
-            ,[people_id]
+            `,
+            [people_id]
         );
 
         const media = result.rows.map(book => ({
@@ -130,15 +141,11 @@ router.get('/book', async (req, res) => {
         console.error('Error executing query:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-}
-);
+});
 
 router.get('/manga', async (req, res) => {
     try {
-        
         const people_id = req.query.people_id;
-
-        
 
         const result = await pool.query(
             `   
@@ -148,19 +155,23 @@ router.get('/manga', async (req, res) => {
                     media.poster_path,
                     media.rating,
                     media.vote_count,
-                    (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+                    COALESCE(fav.is_favorite, 0) AS is_favorite
                 FROM
                     "Fiction Profile"."MEDIA" media
+                LEFT JOIN (
+                    SELECT media_id, 1 AS is_favorite
+                    FROM "Fiction Profile"."FAVORITE"
+                    WHERE user_id = $1
+                ) fav ON fav.media_id = media.media_id
                 WHERE
                     media.manga_id IS NOT NULL
                     AND media.poster_path IS NOT NULL
                 ORDER BY
                     media.popularity DESC, media.rating DESC, media.vote_count DESC
                 LIMIT 8
-            `
-            ,[people_id]
+            `,
+            [people_id]
         );
-
 
         const media = result.rows.map(manga => ({
             id: manga.manga_id,
@@ -176,7 +187,183 @@ router.get('/manga', async (req, res) => {
         console.error('Error executing query:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-}
-);
+});
+
+
+
+// router.get('/movie', async (req, res) => {
+//     try {
+//         const people_id = req.query.people_id;
+
+//         const result = await pool.query(
+//             `   
+//                 SELECT
+//                     media.movie_id,
+//                     media.title,
+//                     media.poster_path,
+//                     media.rating,
+//                     media.vote_count,
+//                     (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+//                 FROM
+//                     "Fiction Profile"."MEDIA" media
+//                 WHERE
+//                     media.movie_id IS NOT NULL
+//                     AND media.poster_path IS NOT NULL
+//                 ORDER BY
+//                     media.popularity DESC, media.rating DESC, media.vote_count DESC
+//                 LIMIT 8
+//             `
+//             ,[people_id]
+//         ); 
+
+//         const media = result.rows.map(mediaItem => ({
+//             id: mediaItem.movie_id,
+//             title: mediaItem.title,
+//             poster_path: `https://image.tmdb.org/t/p/original/${mediaItem.poster_path}`,
+//             rating: mediaItem.rating,
+//             vote_count: mediaItem.vote_count,
+//             is_favorite: mediaItem.is_favorite
+//         }));
+        
+
+//         res.json({ media });
+//     } catch (error) {
+//         console.error('Error executing query:', error);
+//         res.status(500).json({ error: 'Internal server error' });
+//     }
+// });
+
+
+
+
+// router.get('/tv', async (req, res) => {
+//     try {
+        
+//         const people_id = req.query.people_id;
+
+//         const result = await pool.query(
+//             `   
+//                 SELECT
+//                     media.tv_id,
+//                     media.title,
+//                     media.poster_path,
+//                     media.rating,
+//                     media.vote_count,
+//                     (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+//                 FROM
+//                     "Fiction Profile"."MEDIA" media
+//                 WHERE
+//                     media.tv_id IS NOT NULL
+//                 ORDER BY
+//                     media.popularity DESC, media.rating DESC, media.vote_count DESC
+//                 LIMIT 8
+//             `
+//             ,[people_id]
+//         );
+
+//         const media = result.rows.map(tvshow => ({
+//             id: tvshow.tv_id,
+//             title: tvshow.title,
+//             poster_path: `https://image.tmdb.org/t/p/original/${tvshow.poster_path}`,
+//             rating: tvshow.rating,
+//             vote_count: tvshow.vote_count,
+//             is_favorite: tvshow.is_favorite
+//         }));
+
+//         res.json({ media });
+//     } catch (error) {
+//         console.error('Error executing query:', error);
+//         res.status(500).json({ error: 'Internal server error' });
+//     }
+// });
+
+
+// router.get('/book', async (req, res) => {
+//     try {
+
+//         const people_id = req.query.people_id;
+    
+//         const result = await pool.query(
+//             `   
+//                 SELECT
+//                     media.book_id,
+//                     media.title,
+//                     media.poster_path,
+//                     media.rating,
+//                     media.vote_count,
+//                     (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+//                 FROM
+//                     "Fiction Profile"."MEDIA" media
+//                 WHERE
+//                     media.book_id IS NOT NULL
+//                 ORDER BY
+//                     media.popularity DESC, media.rating DESC, media.vote_count DESC
+//                 LIMIT 8
+//             `
+//             ,[people_id]
+//         );
+
+//         const media = result.rows.map(book => ({
+//             id: book.book_id,
+//             title: book.title,
+//             poster_path: book.poster_path,
+//             rating: book.rating,
+//             vote_count: book.vote_count,
+//             is_favorite: book.is_favorite
+//         }));
+
+//         res.json({ media });
+//     } catch (error) {
+//         console.error('Error executing query:', error);
+//         res.status(500).json({ error: 'Internal server error' });
+//     }
+// }
+// );
+
+// router.get('/manga', async (req, res) => {
+//     try {
+        
+//         const people_id = req.query.people_id;
+
+        
+
+//         const result = await pool.query(
+//             `   
+//                 SELECT
+//                     media.manga_id,
+//                     media.title,
+//                     media.poster_path,
+//                     media.rating,
+//                     media.vote_count,
+//                     (SELECT COUNT(*) FROM "Fiction Profile"."FAVORITE" WHERE user_id = $1 AND media_id = media.media_id) AS is_favorite
+//                 FROM
+//                     "Fiction Profile"."MEDIA" media
+//                 WHERE
+//                     media.manga_id IS NOT NULL
+//                     AND media.poster_path IS NOT NULL
+//                 ORDER BY
+//                     media.popularity DESC, media.rating DESC, media.vote_count DESC
+//                 LIMIT 8
+//             `
+//             ,[people_id]
+//         );
+
+
+//         const media = result.rows.map(manga => ({
+//             id: manga.manga_id,
+//             title: manga.title,
+//             poster_path: manga.poster_path,
+//             rating: manga.rating,
+//             vote_count: manga.vote_count,
+//             is_favorite: manga.is_favorite
+//         }));
+
+//         res.json({ media });
+//     } catch (error) {
+//         console.error('Error executing query:', error);
+//         res.status(500).json({ error: 'Internal server error' });
+//     }
+// }
+// );
 
 module.exports = router;
